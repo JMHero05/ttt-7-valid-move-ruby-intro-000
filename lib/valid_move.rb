7ababf56f98c0fd07c2cbbd09f25a6c0a69b4667
Jake Mills <jake.mills.05@gmail.com>
2020-06-21 01:18:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,11 +12,11 @@
 # end
 
 def valid_move?(board, index)
-  # if index.between?(1,9)
-  #   if !position_taken?(board, index)
-  #     true
-  #   end
-  # end
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true
+    end
+  end
 
   position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
   index.between?(0,8) && !position_taken?(board, index)
